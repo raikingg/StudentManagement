18019746f0d24e594dff92175824bea0149896fd
rutwik1998 <ruthvik456@gmail.com>
2023-04-25 12:22:12 +0530

    Committed

--- src/main/java/com/students/StudentManagement/StudentManagementApplication.java
+++ src/main/java/com/students/StudentManagement/StudentManagementApplication.java
@@ -2,8 +2,11 @@ package com.students.StudentManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StudentManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
